02f37c937c72b5f9567ec765562e3758adbc60f5
Intikhab Alam <intikhabalam@eurofins.com>
2017-06-22 12:25:24 +0530

    dddd

---
+++ HelloApplicationTests/MessageTests.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using HelloApplication;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HelloApplication.Tests
+{
+    [TestClass()]
+    public class MessageTests
+    {
+        [TestMethod, ExpectedException(typeof(Exception))]
+        public void DisplayTest_WhenMessageNull()
+        {
+            Message msg = new Message(null);
+            msg.Display();
+        }
+
+
+        [TestMethod]
+        public void DisplayTest_WhenMessageNotNull()
+        {
+            Message msg = new Message("hi");
+            msg.Display();
+        }
+    }
+}
