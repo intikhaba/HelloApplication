e6874451e417ebf4134c4536f7aa630c5086800c
Intikhab Alam <intikhabalam@eurofins.com>
2017-06-22 12:27:11 +0530

    dddd

---
+++ HelloApplication/Message.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace HelloApplication
+{
+    public class Message
+    {
+        private string _message;
+
+        public Message(string _message)
+        {
+            this._message = _message;
+        }
+
+        public string Display()
+        {
+            if (string.IsNullOrEmpty(_message))
+            {
+                throw new System.Exception("No message specified.");
+            }
+            else
+            {
+                return _message;
+            }
+        }
+    }
+}
---
+++ HelloApplication/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HelloApplication
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Message msg = new Message("Hi");
+            Console.WriteLine(msg.Display());
+            Console.ReadKey();
+        }
+    }
+}
